5d3497673b3694134126f696f1fbe2187c27e719
marigold <phelixgluy@gmail.com>
2026-07-02 15:03:00 +0600

    add: zqjs cli building step in build.zig

---
+++ build.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const zqjsExe = b.addExecutable(.{
+        .name = "zqjs",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("zig/zqjs.zig"),
+            .target = target,
+            .optimize = optimize,
+            .single_threaded = true,
+        }),
+    });
+    b.installArtifact(zqjsExe);
+}
