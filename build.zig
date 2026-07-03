edbe1ce420d6c048354dd3212244273326fa8733
marigold <phelixgluy@gmail.com>
2026-07-03 15:59:35 +0600

    add: quickjs files installing build step in main build.zig

--- build.zig
+++ build.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
+const constants = @import("zig/constants.zig");
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const installStep = b.getInstallStep();
 
     const zqjsExe = b.addExecutable(.{
         .name = "zqjs",
@@ -22,17 +25,27 @@ pub fn build(b: *std.Build) void {
 
     b.step("check", "Build on save").dependOn(&zqjsCheck.step);
 
-    const translateQjs = b.addTranslateC(.{
-        // 'quickjs.h' and 'quickjs-libc.h' are all needs for binding
-        // Take only 'quickjs-libc.h' 'cause it includes 'quickjs.h'
+    const translateQuickjs = b.addTranslateC(.{
+        // 'quickjs.h' and 'quickjs-libc.h' are all needs for binding,
+        // So take only 'quickjs-libc.h' 'cause it includes 'quickjs.h'
         .root_source_file = b.path("quickjs-libc.h"),
         .target = target,
         .optimize = optimize,
+
         .link_libc = true,
     });
 
-    b.getInstallStep().dependOn(&b.addInstallFile(
-        translateQjs.getOutput(),
+    installStep.dependOn(&b.addInstallFile(
+        translateQuickjs.getOutput(),
+
         "qjs.zig",
     ).step);
+
+    const installQuickjsFiles = b.step("install-quickjs", "Install quickjs source C files to output");
+
+    for (constants.QuickJsFileNames) |name| {
+        installQuickjsFiles.dependOn(&b.addInstallFile(b.path(name), name).step);
+    }
+
+    installStep.dependOn(installQuickjsFiles);
 }
