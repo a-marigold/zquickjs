370962899a5b93a126cef2e1d95df1a58851e1f7
marigold <phelixgluy@gmail.com>
2026-07-03 22:45:14 +0600

    add: builz.zig.zon

--- zig/constants.zig
+++ zig/constants.zig
@@ -1,7 +1,18 @@
 /// Paths to files (helpers) in the directory where 'zqjs' is located
-pub const exeDirPaths = struct {
+pub const ExeDirPaths = struct {
     pub const buildFile = "bld.zig";
     pub const qjsBindings = "qjs.zig";
     pub const zqjsStd = "std.zig";
-    pub const qjsExe = "qjs.exe";
+    pub const qjscExe = "qjs.exe";
+};
+
+/// Names of files used by 'zqjs' for compilation to static library
+pub const QuickJsFileNames = [_][]const u8{
+    "quickjs.c",
+    "quickjs-libc.c",
+    "cutils.c",
+    "dtoa.c",
+
+    "libregexp.c",
+    "libunicode.c",
 };
